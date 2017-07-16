b6fb9cb2357ef9cf710d6fd8937185a7f2342b4a
Prathamesh Kavlekar <prathamesh1313@gmail.com>
2017-07-16 16:29:06 +0530

    commiting code for functions : makeCacheMatrix, cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions illustrate the use of R scoping rules to cache potentially time consuming computations
 
-## Write a short comment describing this function
+## function to create a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+
+    x_inverse <- NULL
+
+    set <- function(y){       
+        x <<- y
+        x_inverse <<- NULL    
+    }
+
+    get <- function() x
+
+    setInverse <- function(inverse) x_inverse <<- inverse
+
+    getInverse <- function() x_inverse
+
+    list(set = set,get = get,setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    x_inverse <- x$getInverse()
+    
+    if(!is.null(x_inverse)){
+        print("Getting cached inverse.")
+        return(x_inverse)
+    }
+    
+    data <- x$get()
+    
+    print("calculating x inverse.")
+    
+    inverse <- solve(data)
+    
+    #cache the computed value for inverse
+    x$setInverse(inverse)
+    
+    inverse
+
 }
